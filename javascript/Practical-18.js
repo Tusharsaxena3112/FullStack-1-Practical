ff8cfba72c667f9d4f70128ad9f75000ae004aa4
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2020-11-25 15:08:40 +0530

    checking passwords should be same

--- javascript/Practical-18.js
+++ javascript/Practical-18.js
@@ -14,9 +14,16 @@ function checkValid() {
   var userMsg = document.getElementById("user-msg");
   var nameFails = document.getElementById("name-fails");
   var nameCheck = document.getElementById("name-check");
-  var emailError = document.getElementById('email-error');
-  var emailFails = document.getElementById('email-fails');
-  var emailcheck = document.getElementById('email-check');
+  var emailError = document.getElementById("email-error");
+  var passwordError = document.getElementById("pass-error");
+  var emailFails = document.getElementById("email-fails");
+  var emailcheck = document.getElementById("email-check");
+  var passwordFails = document.getElementById("password-fails");
+  var passwordCheck = document.getElementById("password-check");
+  var confirm = document.getElementById("confirm-password");
+  var confirmError = document.getElementById("confirm-error");
+  var confirmFails = document.getElementById("confirm-fails");
+  var confirmCheck = document.getElementById("confirm-check");
   if (username.value.trim() === "") {
     username.style.borderBottom = "1px red solid";
     userMsg.innerHTML = "* User Name cannot be Empty";
@@ -38,5 +45,35 @@ function checkValid() {
     emailFails.style.visibility = "hidden";
     emailcheck.style.visibility = "visible";
     email.style.borderBottom = "1px green solid";
+  }
+  if (password.value.trim() === "") {
+    password.style.borderBottom = "1px red solid";
+    passwordError.innerHTML = "* Password cannot be Empty";
+    passwordError.style.visibility = "visible";
+    passwordFails.style.visibility = "visible";
+  } else {
+    passwordError.style.visibility = "hidden";
+    passwordFails.style.visibility = "hidden";
+    passwordCheck.style.visibility = "visible";
+    password.style.borderBottom = "1px green solid";
+  }
+  if (confirm.value.trim() === "") {
+    confirm.style.borderBottom = "1px red solid";
+    confirmError.innerHTML = "* Password cannot be Empty";
+    confirmError.style.visibility = "visible";
+    confirmFails.style.visibility = "visible";
+  } else {
+    if (confirm.value === password.value) {
+      confirmError.style.visibility = "hidden";
+      confirmFails.style.visibility = "hidden";
+      confirmCheck.style.visibility = "visible";
+      confirm.style.borderBottom = "1px green solid";
+    } else {
+      confirm.style.borderBottom = "1px red solid";
+      confirmError.innerHTML = "* Password Should be same as above";
+      confirmError.style.visibility = "visible";
+      confirmFails.style.visibility = "visible";
+      confirmCheck.style.visibility = 'hidden';
+    }
   }
 }
